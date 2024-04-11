82b45ca849113f055aab1b26b11188a6fe598d83
尾上亮太 <>
2024-04-11 18:08:33 +0900

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/portfolio2024/",
+  base: "/test2/",
   plugins: [react()],
 });
